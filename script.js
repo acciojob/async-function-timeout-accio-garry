c6d524ac06bab4a681b5ab44b8b123d6d3faf6ab
accio-garry <168645773+accio-garry@users.noreply.github.com>
2024-05-19 17:59:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click", ()=>{
+
+let userText = document.getElementById("text").value;
+let userDelay = document.getElementById("delay").value;
+	 
+	setTimeout(()=>{
+		document.getElementById("output").innerHTML= userText;
+	},userDelay)
+});
+
+
